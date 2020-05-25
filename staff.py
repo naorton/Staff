921e9d5932397f3c9d29ad9a1d226250d66fd31b
naorton <naorton@gmail.com>
2020-05-25 15:43:32 -0500

    Update staff.py

--- staff.py
+++ staff.py
@@ -7,7 +7,7 @@ time.sleep(1) # delay necessary to allow mpu9250 to settle
 print('recording data')
 while 1:
     try:
-        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+        ax,ay,az,wx,wy,wz,temp = mpu6050_conv() # read and convert mpu6050 data
         mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
     except:
         continue
@@ -17,4 +17,5 @@ while 1:
     print('gyro [dps]:  x = , y = , z = '.format(wx,wy,wz))
     print('mag [uT]:   x = , y = , z = '.format(mx,my,mz))
     print('{}'.format('-'*30))
+    print('temp: ' + temp)
     time.sleep(1)
